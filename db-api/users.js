e555a54120b14ace5d31ac8a86b672717f17f9b4
Ayelen <ayelenrizzalli@gmail.com>
2018-09-06 11:22:41 -0300

    Find all users

--- db-api/users.js
+++ db-api/users.js
@@ -3,5 +3,9 @@ import {Users as UsersModel} from '../models'
 export default {
   createUser : function (userObject) {
     return UsersModel.create(userObject);
+  },
+
+  findAll : function (){
+    return UsersModel.find();
   }
 }
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,18 @@ var router = express.Router();
 import {handleError} from '../utils';
 import {Users} from '../db-api'
 
+router.get('/', async function(req, res) {
+  try {
+    let users = await Users.findAll();
+    res.status(200).send({
+      success: true,
+      users: users
+    });
+  } catch (error) {
+    handleError(error, res);
+  }
+})
+
 router.post('/', async function(req, res) {
   let userObject = {
     name: req.body.name,
